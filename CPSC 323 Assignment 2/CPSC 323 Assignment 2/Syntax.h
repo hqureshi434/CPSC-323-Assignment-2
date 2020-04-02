e285e8664ddf394cfff4964de500496d50d16f7f
hqureshi434 <hqureshi434@gmail.com>
2020-04-02 16:57:44 -0700

    Added the Syntax header file

---
+++ CPSC 323 Assignment 2/CPSC 323 Assignment 2/Syntax.h
@@ -0,0 +1,16 @@
+//Recursive Descent Parser
+
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Syntax {
+private:
+
+public:
+	Syntax() {}; //Default Constructor
+
+
+};
